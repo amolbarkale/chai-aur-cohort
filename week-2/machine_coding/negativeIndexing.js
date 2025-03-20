d6f18754cb628897191653ee61995fab408865ab
amolsbGP <amol@gameopedia.com>
2025-03-20 07:33:09 +0530

    learn basics of JS with pollyfills, promises, arrays objects and HTM, CSS.

--- week-2/machine_coding/negativeIndexing.js
+++ week-2/machine_coding/negativeIndexing.js
@@ -1,8 +1,14 @@
 const person = {
   name: "Amol",
   age: 32,
-  password: 123,
+  password: function () {
+    console.log("function:", this.name);
+  },
 };
+
+// this is a deep copy
+const newPersonn = JSON.stringify(person);
+new Object(newPersonn);
 
 const newPerson = new Proxy(person, {
   get(target, prop) {
@@ -11,10 +17,12 @@ const newPerson = new Proxy(person, {
     return target[prop];
   },
 });
+
 //_______________________________________________________________________________________________________//
 
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 
+// function negativeIndex([...arr]) to prevent shallow copy of original array
 function negativeIndex(arr) {
   const target = new Proxy(arr, {
     // getter: to get the value
@@ -42,9 +50,9 @@ function negativeIndex(arr) {
 }
 
 const proxyArray = negativeIndex(arr);
-proxyArray[-1];
-// console.log("proxyArray[-2]:", proxyArray[-2]);
 
 proxyArray[-1] = 100;
+
 // console.log("proxyArray:", proxyArray);
-// console.log("arr:", arr);
+// console.log("arr[-1]:", arr[-1]);
+//_______________________________________________________________________________________________________//
